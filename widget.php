d297c69c83eeb3c9a2bdba63b7a2b7e3f4ab936c
bkalish <bkalish@gmail.com>
2015-03-31 16:53:51 -0400

    added empty widget template

---
+++ widget.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Staff Picks widget.
+ */
+
+class Staff_Picks_Widget extends WP_Widget {
+
+  /**
+   * Sets up the widgets name etc
+   */
+  public function __construct() {
+    // widget actual processes
+  }
+
+  /**
+   * Outputs the content of the widget
+   *
+   * @param array $args
+   * @param array $instance
+   */
+  public function widget( $args, $instance ) {
+    // outputs the content of the widget
+  }
+
+  /**
+   * Outputs the options form on admin
+   *
+   * @param array $instance The widget options
+   */
+  public function form( $instance ) {
+    // outputs the options form on admin
+  }
+
+  /**
+   * Processing widget options on save
+   *
+   * @param array $new_instance The new options
+   * @param array $old_instance The previous options
+   */
+  public function update( $new_instance, $old_instance ) {
+    // processes widget options to be saved
+  }
+}
