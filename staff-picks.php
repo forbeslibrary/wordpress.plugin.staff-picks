0659760d618fb8dbec80aa18ed5c96cf55f87c9e
bkalish <bkalish@gmail.com>
2015-06-30 13:15:45 -0400

    added LICENSE.txt. fixes #11

--- staff-picks.php
+++ staff-picks.php
@@ -6,6 +6,8 @@
  * Version: 1.1.1
  * Author: Benjamin Kalish
  * Author URI: https://github.com/bkalish
+ * License: GNU General Public License v2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 require_once(dirname( __FILE__ ) . '/helpers.php');
